a2ad3998f0adc8018f022542a195b059b5121a29
ArthurCrossy <arthurcrossy@gmail.com>
2021-03-25 19:08:46 -0300

    teste eclipse

---
+++ Exercicio01/src/somarDoisNumeros.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class somarDoisNumeros {
+	
+	public static void main(String[] args) {
+		
+		int num1, num2, soma;
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Digite o numero 01:");
+		num1 = input.nextInt();
+		System.out.println("Digite o numero 02:");
+		num2 = input.nextInt();
+		
+		soma = num1 + num2;
+		
+		
+		System.out.println(soma);
+		
+		
+		
+		
+	}
+	
+}
+
